92406b356aed0a7b57fedc0badaf2b94ce8522e6
nguyenhuuthien <nguyenhuuthien88bk@gmail.com>
2026-07-22 17:20:19 +0000

    update

--- commands.sh
+++ commands.sh
@@ -1,15 +1,2 @@
-#1
+#2 +a
 #train-baseline
-eval "$($HOME/miniconda3/bin/conda shell.bash hook)"
-sleep 3
-conda activate sparse_emb
-sleep 3
-
-nvidia-smi
-sleep 3
-
-rm -rf /opt/dlami/nvme/sparse_emb_outputs/baseline
-echo "Previous output removed"
-sleep 3
-
-bash scripts/train_qwen3_0.6b_baseline.sh
