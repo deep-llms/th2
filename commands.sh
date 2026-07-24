dbc040281ae4906b9c9890746389568035f46157
nguyenhuuthien <nguyenhuuthien88bk@gmail.com>
2026-07-24 01:58:42 +0000

    update

--- commands.sh
+++ commands.sh
@@ -1,3 +1,11 @@
-#2 +a 
-#train-baseline
-    
+#1
+#run-dummy
+eval "$($HOME/miniconda3/bin/conda shell.bash hook)"
+sleep 3
+conda activate sparse_emb
+sleep 3
+
+nvidia-smi
+sleep 3
+
+python dummy.py
