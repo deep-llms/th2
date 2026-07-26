600591dfb564988cf90fa63126107e42e2c8d116
nguyenhuuthien <nguyenhuuthien88bk@gmail.com>
2026-07-26 12:29:30 +0000

    update

--- commands.sh
+++ commands.sh
@@ -1,5 +1,12 @@
-#1 +30+a 
-#kill-dummy
-pkill -f "dummy.py"
-sleep 10
-nvidia-smi 
+#1
+#train-ant-experiments
+eval "$($HOME/miniconda3/bin/conda shell.bash hook)"
+sleep 3
+conda activate sparse_emb
+sleep 3
+
+nvidia-smi
+sleep 3
+
+export WANDB_MODE=offline
+python run_experiments.py --stop-at-step 10000 --log-dir /opt/dlami/nvme/sparse_emb_outputs/logs
