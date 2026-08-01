78165593f4e0923bfe3b061583ca6618bda7efbe
nguyenhuuthien <nguyenhuuthien88bk@gmail.com>
2026-08-01 08:50:47 +0000

    update

--- commands.sh
+++ commands.sh
@@ -1,12 +1,2 @@
-#1
+#2 +a
 #prepare-data
-eval "$($HOME/miniconda3/bin/conda shell.bash hook)"
-sleep 3
-conda activate sparse_emb
-sleep 3
-
-python prepare_data.py \
-    --raw-dir /opt/dlami/nvme/sparse_emb_data/raw \
-    --data-dir /opt/dlami/nvme/sparse_emb_data \
-    --tokenizer-name Qwen/Qwen3-0.6B \
-    --num-workers 4
