87b55bf8e260c0479642d8812d4ce1fac8a97f77
nguyenhuuthien <nguyenhuuthien88bk@gmail.com>
2026-08-01 20:38:27 +0000

    pull mode

--- commands.sh
+++ commands.sh
@@ -1,21 +1,2 @@
-#1
-#train-original-ant
-eval "$($HOME/miniconda3/bin/conda shell.bash hook)"
-sleep 3
-conda activate sparse_emb
-sleep 3
-
-nvidia-smi
-sleep 3
-
-# Clean previous outputs
-rm -rf /opt/dlami/nvme/sparse_emb_outputs/original_ant
-rm -rf /opt/dlami/nvme/sparse_emb_outputs/logs
-
-# Copy accelerate config
-mkdir -p ~/.cache/huggingface/accelerate
-cp resources/accelerate_config.yaml ~/.cache/huggingface/accelerate/default_config.yaml
-sleep 3
-
-export WANDB_MODE=offline
-python run_experiments.py --experiments 0 --stop-at-step 10000 --log-dir /opt/dlami/nvme/sparse_emb_outputs/logs
+#2 +a
+#pull-log
