b1319a59eb4145671646be82e67dd9f784a074d7
nguyenhuuthien <nguyenhuuthien88bk@gmail.com>
2026-08-25 11:32:02 +0000

    commands: use resolved path for B200 result pull

--- commands.sh
+++ commands.sh
@@ -1,2 +1,2 @@
-#2 +a -f-/mnt/local/_outputs/@PROJECT@/result_exports/all_experiment_results_20260825.tar.gz,/mnt/local/_outputs/@PROJECT@/result_exports/all_experiment_results_20260825.tar.gz.sha256,/mnt/local/_outputs/@PROJECT@/result_exports/all_experiment_results_20260825.files
-#th2-pull-all-compact-b200-results-20260825
+#2 +a -f-/mnt/local/_outputs/deep-llms_th2/result_exports/all_experiment_results_20260825.tar.gz,/mnt/local/_outputs/deep-llms_th2/result_exports/all_experiment_results_20260825.tar.gz.sha256,/mnt/local/_outputs/deep-llms_th2/result_exports/all_experiment_results_20260825.files
+#th2-pull-all-compact-b200-results-resolved-path-20260825
