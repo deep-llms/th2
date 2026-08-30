65158b350515bc847407ba987f27c57910862e37
nguyenhuuthien <nguyenhuuthien88bk@gmail.com>
2026-08-30 21:51:52 +0000

    Use explicit th2 paths for result pull

--- commands.sh
+++ commands.sh
@@ -1,2 +1,2 @@
-#2 +a -f-/mnt/local/_outputs/@PROJECT@/result_exports/phase1_four_model_eval_finetune_results_20260830.tar.gz,/mnt/local/_outputs/@PROJECT@/result_exports/phase1_four_model_eval_finetune_results_20260830.tar.gz.sha256,/mnt/local/_outputs/@PROJECT@/result_exports/phase1_four_model_eval_finetune_results_20260830.files
-#th2-pull-four-model-eval-finetune-results-20260830-a01
+#2 +a -f-/mnt/local/_outputs/deep-llms_th2/result_exports/phase1_four_model_eval_finetune_results_20260830.tar.gz,/mnt/local/_outputs/deep-llms_th2/result_exports/phase1_four_model_eval_finetune_results_20260830.tar.gz.sha256,/mnt/local/_outputs/deep-llms_th2/result_exports/phase1_four_model_eval_finetune_results_20260830.files
+#th2-pull-four-model-eval-finetune-results-20260830-a02
