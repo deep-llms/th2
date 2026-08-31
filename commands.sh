cb273729b65956a4325808c25c4a66d490c48868
nguyenhuuthien <nguyenhuuthien88bk@gmail.com>
2026-08-31 18:08:30 +0000

    commands: use resolved path for RSE traceback

--- commands.sh
+++ commands.sh
@@ -1,2 +1,2 @@
-#2 +a -f-/mnt/local/_outputs/@PROJECT@/residual_subspace_experts_prodshape_smoke_20260831_a01/train.log
-#th2-pull-rse-prodshape-smoke-a01-train-log-20260831
+#2 +a -f-/mnt/local/_outputs/deep-llms_th2/residual_subspace_experts_prodshape_smoke_20260831_a01/train.log
+#th2-pull-rse-prodshape-smoke-a01-train-log-20260831-a02
