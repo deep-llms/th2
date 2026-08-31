aa299c081d6564794c74dd07c5dfb7224d29e3cb
nguyenhuuthien <nguyenhuuthien88bk@gmail.com>
2026-08-31 21:11:26 +0000

    Refresh live RSE retry log

--- commands.sh
+++ commands.sh
@@ -1,2 +1,2 @@
 #2 +a -f-/mnt/local/_outputs/deep-llms_th2/logs/final_rse_b8a8_hashed_10k_20260831_a02/residual_subspace_experts_tied_g12_r120_q80.log
-#th2-pull-live-rse-b8a8-log-20260831-a01
+#th2-pull-live-rse-b8a8-log-20260831-a02
