eb88c3837066571ea96e42a02cdd18fa9ae76b79
nguyenhuuthien <nguyenhuuthien88bk@gmail.com>
2026-08-31 20:41:20 +0000

    commands: pull live Hashed training log

--- commands.sh
+++ commands.sh
@@ -1,2 +1,2 @@
-#2 +a -f-/mnt/local/_outputs/deep-llms_th2/logs/final_rse_hashed_10k_20260831_a01/residual_subspace_experts_tied_g12_r120_q80.log
-#th2-pull-final-rse-failure-log-20260831-a01
+#2 +a -f-/mnt/local/_outputs/deep-llms_th2/logs/final_rse_hashed_10k_20260831_a01/product_code_hashed_h2048.log
+#th2-pull-final-hashed-live-log-20260831-a01
