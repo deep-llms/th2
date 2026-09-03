07009df362552c5ed118ca23bc56ac2754a58ac3
nguyenhuuthien <nguyenhuuthien88bk@gmail.com>
2026-09-03 14:42:07 +0000

    refresh sequential training progress again

--- commands.sh
+++ commands.sh
@@ -1,5 +1,5 @@
 #1 +60+a
-#th2-readonly-progress-three-experiments-20260903-a06
+#th2-readonly-progress-three-experiments-20260903-a07
 set -euo pipefail
 
 TASK_OUTPUT_BASE=/mnt/local/_outputs/@PROJECT@
