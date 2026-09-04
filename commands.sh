da5fc5ee956de393a894c5fc4a35ee89eb33e416
nguyenhuuthien <nguyenhuuthien88bk@gmail.com>
2026-09-04 15:34:42 +0000

    check five-checkpoint workflow completion

--- commands.sh
+++ commands.sh
@@ -1,5 +1,5 @@
 #1 +240+a
-#th2-readonly-check-five-checkpoint-eval-finetune-completion-20260904-a03
+#th2-readonly-check-five-checkpoint-eval-finetune-completion-20260904-a04
 set -u
 
 TASK_OUTPUT_BASE=/mnt/local/_outputs/@PROJECT@
