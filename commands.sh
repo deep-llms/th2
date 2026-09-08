39c200aa45b9f484e6bd263eceeb0dbe1511aa23
nguyenhuuthien <nguyenhuuthien88bk@gmail.com>
2026-09-08 16:30:37 +0000

    Export completed batch benchmark measurements

--- commands.sh
+++ commands.sh
@@ -1,2 +1,2 @@
-#2 +a
-#th2-swt-readonly-batch-benchmark-progress-20260908-1629-a01
+#2 +a -f-/mnt/local/_outputs/deep-llms_th2/swt/batch_benchmark_20260908_a01/benchmark_complete.json
+#th2-swt-pull-batch-benchmark-results-20260908-1630-a01
