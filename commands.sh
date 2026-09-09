12b0ecb0771b4fe1633e679b1df33f8b13bea638
nguyenhuuthien <nguyenhuuthien88bk@gmail.com>
2026-09-09 03:43:27 +0000

    Retrieve six-arm 5k completion and burn handoff evidence

--- commands.sh
+++ commands.sh
@@ -1,2 +1,2 @@
-#2 +a -f-/mnt/local/_outputs/deep-llms_th2/swt/qwen6_allarms_5k_s42_20260908_a01/pipeline.log
-#th2-swt-pull-5k-training-progress-20260908-1853-a01
+#2 +a -f-/mnt/local/_outputs/deep-llms_th2/swt/qwen6_allarms_5k_s42_20260908_a01/pipeline.log,/mnt/local/_outputs/deep-llms_th2/swt/qwen6_allarms_5k_s42_20260908_a01/training_complete.json,/mnt/local/_outputs/deep-llms_th2/swt/qwen6_allarms_5k_s42_20260908_a01/burn_verified.json
+#th2-swt-readonly-pull-training-completion-20260909-a01
