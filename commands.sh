1ae3f345ab1bad8f2004f7ce8ae6a5ca7644bdf1
nguyenhuuthien <nguyenhuuthien88bk@gmail.com>
2026-09-09 21:09:51 +0000

    Export next architecture smoke verification

--- commands.sh
+++ commands.sh
@@ -1,2 +1,2 @@
-#2 +a
-#th2-swt-pull-next-architecture-production-smoke-progress-20260909-a03
+#2 +a -f-/mnt/local/_outputs/deep-llms_th2/swt/next_architectures_smoke_20260909_a03/production_ddp_smoke.json,/mnt/local/_outputs/deep-llms_th2/swt/burn_after_next_architectures_smoke_20260909_a03/burn_verified.json
+#th2-swt-export-next-architecture-smoke-and-burn-verification-20260909-a03
