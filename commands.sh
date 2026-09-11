0b33116347b0aad8f4321fb27d33db67f9d20e41
nguyenhuuthien <nguyenhuuthien88bk@gmail.com>
2026-09-11 15:26:02 +0000

    Install train_env and eval on replacement machine

--- commands.sh
+++ commands.sh
@@ -1,2 +1,2 @@
-#2 +a -f-/mnt/local/_outputs/deep-llms_th2/data_preparation/culturax_qwen_20260911_a01/sampling.log,/mnt/local/_outputs/deep-llms_th2/data_preparation/culturax_qwen_20260911_a01/sampling_complete.json
-#th2-pull-sampling-progress-20260911-a01
+#i envs/train_env.txt envs/eval.txt +a
+#th2-install-train-env-and-eval-replacement-machine-20260911-a02
