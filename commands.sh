a436b0c4eb29521d93441bc697df26155698cf0a
nguyenhuuthien <nguyenhuuthien88bk@gmail.com>
2026-09-13 02:37:26 +0000

    Retrieve overnight pilot status 20260913T023725Z

--- commands.sh
+++ commands.sh
@@ -1,2 +1,2 @@
-#2 +a
-#th2-ccm-record-armed-overnight-handoff-20260913-a01
+#2 +a -f-/mnt/local/_outputs/deep-llms_th2/ccm_pilot_seed17_20260913_a01.handoff.log,/mnt/local/_outputs/deep-llms_th2/ccm_pilot_seed17_20260913_a01/status.json,/mnt/local/_outputs/deep-llms_th2/ccm_prepare_pilot_v1_20260913_a01/prepare.log
+#th2-ccm-hourly-20260913T023725Z
