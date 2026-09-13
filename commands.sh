04a4728ef949e06095c35718c7b45e8709097e2b
nguyenhuuthien <nguyenhuuthien88bk@gmail.com>
2026-09-13 20:27:45 +0000

    Read current Stage-2 progress for completion estimate

--- commands.sh
+++ commands.sh
@@ -1,2 +1,2 @@
-#2 +a -f-/mnt/local/_outputs/deep-llms_th2/ccm_stage2_seed17_20260913_a01.handoff.log,/mnt/local/_outputs/deep-llms_th2/ccm_stage2_seed17_20260913_a01/status.json,/mnt/local/_outputs/deep-llms_th2/ccm_stage2_seed17_20260913_a01/train/base/run.json,/mnt/local/_outputs/deep-llms_th2/ccm_stage2_seed17_20260913_a01/train/base/train.jsonl
-#th2-ccm-stage2-confirm-updates-20260913-a02
+#2 +a -f-/mnt/local/_outputs/deep-llms_th2/ccm_stage2_seed17_20260913_a01.handoff.log,/mnt/local/_outputs/deep-llms_th2/ccm_stage2_seed17_20260913_a01/status.json
+#th2-ccm-stage2-progress-eta-20260913-2027
