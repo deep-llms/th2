3568b0d32a42513afdbff0c8ac5c94bff55b387e
nguyenhuuthien <nguyenhuuthien88bk@gmail.com>
2026-09-13 00:55:44 +0000

    Retrieve CCM smoke terminal handoff log

--- commands.sh
+++ commands.sh
@@ -1,2 +1,2 @@
-#2 +a -f-/mnt/local/_outputs/deep-llms_th2/ccm_smoke_20260913_a01/payload.log,/mnt/local/_outputs/deep-llms_th2/ccm_smoke_20260913_a01/pipeline_gpu0.log
-#th2-ccm-pull-smoke-progress-20260913-a01
+#2 +a
+#th2-ccm-pull-smoke-handoff-20260913-a01
