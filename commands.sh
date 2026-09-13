08b0f21d69b3b66f161f4cefefa537d8c3379352
nguyenhuuthien <nguyenhuuthien88bk@gmail.com>
2026-09-13 17:45:25 +0000

    Check Stage-1 evaluation completion and burn status

--- commands.sh
+++ commands.sh
@@ -1,2 +1,2 @@
 #2 +a -f-/mnt/local/_outputs/deep-llms_th2/ccm_stage1_seed17_20260913_a01.handoff.log,/mnt/local/_outputs/deep-llms_th2/ccm_stage1_seed17_20260913_a01/status.json
-#th2-ccm-stage1-progress-20260913-1622
+#th2-ccm-stage1-completion-check-20260913-1745
