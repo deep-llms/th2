dfe0c85b1e49a2b9636bbd41f1e30a4839a228e7
nguyenhuuthien <nguyenhuuthien88bk@gmail.com>
2026-09-13 14:52:24 +0000

    Check first Stage-1 optimizer updates and recorded run contract

--- commands.sh
+++ commands.sh
@@ -1,2 +1,2 @@
-#2 +a -f-/mnt/local/_outputs/deep-llms_th2/ccm_stage1_seed17_20260913_a01.handoff.log,/mnt/local/_outputs/deep-llms_th2/ccm_stage1_seed17_20260913_a01/status.json
-#th2-ccm-stage1-confirm-training-20260913-a01
+#2 +a -f-/mnt/local/_outputs/deep-llms_th2/ccm_stage1_seed17_20260913_a01.handoff.log,/mnt/local/_outputs/deep-llms_th2/ccm_stage1_seed17_20260913_a01/status.json,/mnt/local/_outputs/deep-llms_th2/ccm_stage1_seed17_20260913_a01/train/contextual/run.json,/mnt/local/_outputs/deep-llms_th2/ccm_stage1_seed17_20260913_a01/train/contextual/train.jsonl
+#th2-ccm-stage1-confirm-updates-20260913-a02
