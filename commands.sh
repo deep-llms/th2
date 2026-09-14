9e2ac823cff760d5772645ac89af8ad67453fac7
nguyenhuuthien <nguyenhuuthien88bk@gmail.com>
2026-09-14 10:58:16 +0000

    Pull seed29 completion gates and current burn heartbeat

--- commands.sh
+++ commands.sh
@@ -1,2 +1,2 @@
-#2 +a -f-/mnt/local/_outputs/deep-llms_th2/ccm_pilot_seed29_20260913_a01.handoff.log,/mnt/local/_outputs/deep-llms_th2/ccm_pilot_seed29_20260913_a01/status.json,/mnt/local/_outputs/deep-llms_th2/ccm_stage2_seed17_20260913_a01/status.json,/mnt/local/_outputs/deep-llms_th2/ccm_stage2_seed17_20260913_a01.handoff.log
-#th2-ccm-verify-seed29-queue-and-stage2-20260913-a01
+#2 +a -f-/mnt/local/_outputs/deep-llms_th2/ccm_pilot_seed29_20260913_a01.handoff.log,/mnt/local/_outputs/deep-llms_th2/ccm_pilot_seed29_20260913_a01/status.json,/mnt/local/_outputs/deep-llms_th2/ccm_pilot_seed29_20260913_a01/complete.json,/mnt/local/_outputs/deep-llms_th2/ccm_pilot_seed29_20260913_a01/validated_common.json,/mnt/local/_outputs/deep-llms_th2/ccm_stage2_seed17_20260913_a01/complete.json,/mnt/local/_outputs/deep-llms_th2/ccm_stage2_seed17_20260913_a01/validated_panel.json
+#th2-ccm-seed29-completion-and-burn-check-20260914-a01
